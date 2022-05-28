6bd20ad108f729bccc1dd371f3eb3567908c15e4
Jaime García Camacho <62312151+ELBATTU@users.noreply.github.com>
2022-05-27 20:08:29 -0500

    Add files via upload

---
+++ Analizador_Lexico_Sintactico_LenguajeR/src/codigo/Tokens.java
@@ -0,0 +1,34 @@
+
+package codigo;
+
+public enum Tokens {
+    Linea,
+    Comillas,
+    Print,
+    If,
+    Else,
+    Do,
+    While,
+    For,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Op_logico,
+    Op_relacional,
+    Op_atribucion,
+    Op_booleano,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Corchete_a,
+    Corchete_c,
+    P_coma,
+    Identificador,
+    Numero,
+    Conector,
+    In,
+    ERROR
+}
